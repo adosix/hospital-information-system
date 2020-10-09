1affe336be26697e462f12f1d1e245f38d2c32a0
Branislav Dubec <47944127+BranislavDubec@users.noreply.github.com>
2020-10-09 21:50:14 +0200

    filling values into table

--- db/fill_data.sql
+++ db/fill_data.sql
@@ -10,17 +10,41 @@ INSERT INTO hospital_information_system_app_user (First_name,Last_name,Password,
 INSERT INTO hospital_information_system_app_user (First_name,Last_name,Password,Email, State, City, Street)
     VALUES("Filip","Trnka", "kdedomovmuj", "paranoid@volimkotlebu.sk","Maďarsko", "Budapešť","Ištenem");
 INSERT INTO hospital_information_system_app_user (First_name,Last_name,Password,Email, State, City, Street)
-    VALUES("Matus","Blahut", "matus999", "ratata@gmail.sk","Slobakia", "Zilina","Lipova");
+    VALUES("Matus","Blahut", "matus999", "ratata@gmail.sk","Slobakia", "Žilina","Lipova");
 INSERT INTO hospital_information_system_app_user (First_name,Last_name,Password,Email, State, City, Street)
-    VALUES("Matus","Blahut", "matus999", "ratata@gmail.sk","Slobakia", "Zilina","Lipova");
+    VALUES("Matus","Surovčák", "surkolubibronku", "surko69@gmail.sk","Slovakia", "Žilina","Lucna");
+INSERT INTO hospital_information_system_app_user (First_name,Last_name,Password,Email, State, City, Street)
+    VALUES("Martin","Kapitulčin", "xmartinox", "lolko@gmail.sk","Slovakia", "Varín","Lipova");
+INSERT INTO hospital_information_system_app_user (First_name,Last_name,Password,Email, State, City, Street)
+    VALUES("Šimon","Hallo", "babymeta123", "gumenak@gmail.sk","Slovakia", "Partizánske","Horná cesta");
 
+INSERT INTO hospital_information_system_app_user (First_name,Last_name,Password,Email, State, City, Street)
+    VALUES("Andrej","Ježík", "lucka0711", "lucinqa@gmail.sk","Slovakia", "Považská Bystrica","Dolná cesta");
+INSERT INTO hospital_information_system_app_user (First_name,Last_name,Password,Email, State, City, Street)
+    VALUES("Braňo","Dubec", "axinko", "hater85@gmail.sk","Slovakia", "Rajec","Smreková");
+INSERT INTO hospital_information_system_app_user (First_name,Last_name,Password,Email, State, City, Street)
+    VALUES("Boris","Štrbák", "tralala", "strbkogmail.sk","Slovakia", "Púchov","Smreková");  
 INSERT INTO hospital_information_system_app_insurance_worker
     VALUES("10000")
 INSERT INTO hospital_information_system_app_insurance_worker
     VALUES("10001")
 INSERT INTO hospital_information_system_app_insurance_worker
     VALUES("10002")
-INSERT INTO hospital_information_system_app_insurance_worker
+INSERT INTO hospital_information_system_app_doctor
     VALUES("10003")
-INSERT INTO hospital_information_system_app_insurance_worker
+INSERT INTO hospital_information_system_app_doctor
     VALUES("10004")
+INSERT INTO hospital_information_system_app_doctor
+    VALUES("10005")
+INSERT INTO hospital_information_system_app_doctor
+    VALUES("10006")
+INSERT INTO hospital_information_system_app_doctor
+    VALUES("10007")
+INSERT INTO hospital_information_system_app_doctor
+    VALUES("10008")
+INSERT INTO hospital_information_system_app_patient
+    VALUES("10009")
+INSERT INTO hospital_information_system_app_patient
+    VALUES("10010")
+INSERT INTO hospital_information_system_app_patient
+    VALUES("10011")
