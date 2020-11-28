44cd6f57900c447ad0ea09d308eb671e66dd7b18
Andrej <andrejjezik@gmail.com>
2020-11-28 22:22:52 +0100

    start script updated, generating usernames

--- start.sh
+++ start.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
+if [ $1 == "dbinit" ] 
+then
+    db_cleanup=`cat db/db_cleanup.sql`
+    #echo "$db_cleanup"
+    sudo mysql -u djangouser -p -e "$db_cleanup"
+    echo "db_cleanup done"
+
+    db_init=`cat db/db_init.sql`
+    #echo "$db_init"
+    sudo mysql -u djangouser -p -e "$db_init"
+    echo "db_init done"
+
+    fill_data=`cat db/fill_data.sql`
+    #echo "$fill_data"
+    sudo mysql -u djangouser -p -e "$fill_data"
+    echo "db_fill_data done"
+fi
+echo $1
 #activate virtual environment
 source ../Environments/hospital_env/bin/activate
 #check for changes and run server
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver
+
--- users/views.py
+++ users/views.py
@@ -144,6 +144,21 @@ def register(request):
 
             user = u_form.save(commit=False)
             profile=p_form.save(commit=False)
+            
+            #getting username
+            while True:
+                i = 0
+                stop = False
+                user.username = user.last_name[0 : 5]+str(i)
+                for u in  AuthUser.objects.all():
+                    if u.username == user.username:
+                        stop = False
+                        break
+                    stop = True
+                if stop:
+                    break
+                i += 1
+
             max = datetime.now().date()
             min = datetime(1900,1,1).date()
             if(profile.birth_date >= max or profile.birth_date<= min ):
