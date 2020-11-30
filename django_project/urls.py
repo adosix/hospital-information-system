1c02b9e6e37f8dc22d55302689b11c0650264a3b
BranislavDubec <dubec.brano@gmail.com>
2020-11-30 17:22:40 +0100

    backend final

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path, include
+from django.urls import path, include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
